4d070f3b9746bc339fd208438639f396f235da0d
0xb0b1 <vcente82@gmail.com>
2021-02-23 14:37:17 -0300

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { Cicles } from "../components/Cicles";
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -20,7 +21,9 @@ export default function Home() {
                     <CompletedChallenges />
                     <Countdown />
                 </div>
-                <div></div>
+                <div>
+                    <Cicles />
+                </div>
             </section>
         </div>
     );
